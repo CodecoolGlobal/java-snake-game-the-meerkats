60c664dab3963c1f3e13ee113b1338ceb5e8ed24
Silviu Catrina <silviu.catrina@gmail.com>
2020-06-10 22:44:39 +0300

    Add same moving pattern for the enemies

--- src/main/java/com/codecool/snake/entities/enemies/Enemy.java
+++ src/main/java/com/codecool/snake/entities/enemies/Enemy.java
@@ -1,16 +1,64 @@
 package com.codecool.snake.entities.enemies;
 
+import com.codecool.snake.Globals;
+import com.codecool.snake.Utils;
+import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.Interactable;
+import javafx.geometry.Point2D;
 
-public abstract class Enemy extends GameEntity{
+import java.util.Random;
+
+public abstract class Enemy extends GameEntity implements Animatable, Interactable {
     private final int damage;
+    private Point2D heading;
+    public static Random rnd = new Random();
+    private double direction;
+    private int speed;
 
     public Enemy(int damage) {
         this.damage = damage;
+        setEnemyPosition();
+    }
+
+    public Point2D getHeading(Point2D heading) {
+        return this.heading;
+    }
+
+    private void setHeading(Point2D heading) {
+        this.heading = heading;
     }
 
     public int getDamage() {
-        return damage;
+        return this.damage;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    protected void setEnemyPosition() {
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+    }
+
+    public void setFinalHeading(double direction, int speed) {
+        this.direction = direction;
+        this.speed = speed;
+        this.heading =Utils.directionToVector(direction, speed);
+    }
+
+
+    public void step() {
+        if (isOutOfBounds()) {
+            destroy();
+        }
+        setX(getX() + heading.getX());
+        setY(getY() + heading.getY());
     }
 
 }
--- src/main/java/com/codecool/snake/entities/enemies/Lion.java
+++ src/main/java/com/codecool/snake/entities/enemies/Lion.java
@@ -15,29 +15,11 @@ import java.util.Random;
 public class Lion extends Enemy implements Interactable, Animatable {
 
 
-    private Point2D lion;
-    private static Random rnd = new Random();
-
     public Lion() {
         super(20);
 
         setImage(Globals.getInstance().getImage("Lion"));
-        setX(0);
-        setY((rnd.nextDouble() * Globals.WINDOW_HEIGHT - 60) + 30);
-
-        double direction = 90;
-
-        double speed = 0.5;
-        lion = Utils.directionToVector(direction, speed);
-    }
-
-    @Override
-    public void step() {
-        if (isOutOfBounds()) {
-            destroy();
-        }
-        setX(getX() + lion.getX());
-        setY(getY() + lion.getY());
+        setFinalHeading(rnd.nextDouble() * 360, 1);
     }
 
 
