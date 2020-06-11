94005c92dc796adfdc3ebc972e964f9595f0ef58
MihaiBarbu93 <mihai.barbu1993@gmail.com>
2020-06-11 10:54:36 +0300

    Powerups not spawn on snake

--- src/main/java/com/codecool/snake/entities/powerups/HurryPowerUp.java
+++ src/main/java/com/codecool/snake/entities/powerups/HurryPowerUp.java
@@ -3,6 +3,7 @@ package com.codecool.snake.entities.powerups;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeBody;
 import com.codecool.snake.entities.snakes.SnakeHead;
 
 import java.util.Random;
@@ -19,7 +20,7 @@ public class HurryPowerUp extends GameEntity implements Interactable {
 
     @Override
     public void apply(GameEntity entity) {
-        if(entity instanceof SnakeHead){
+        if(entity instanceof SnakeHead || entity instanceof SnakeBody){
             System.out.println(getMessage());
             destroy();
         }
